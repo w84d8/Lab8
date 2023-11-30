28572605536ed9b2f0030e2af2b5e21ce127aa64
{{w84d8}} <{{rakonline99@gmail.com}}>
2023-11-30 15:35:54 +0200

    1

---
+++ Task 1/ConfigurationManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class ConfigurationManager
+{
+    private static ConfigurationManager? instance;
+    private string environmentSetting;
+
+    private ConfigurationManager()
+    {
+        environmentSetting = "Production";
+    }
+
+    public static ConfigurationManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new ConfigurationManager();
+            }
+            return instance;
+        }
+    }
+
+    internal class ConsoleWrapper
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine() ?? string.Empty;
+        }
+    }
+
+    public string EnvironmentSetting
+    {
+        get { return environmentSetting; }
+        set
+        {
+            environmentSetting = value;
+            Console.WriteLine($"Environment setting changed to: {environmentSetting}");
+        }
+    }
+
+}
---
+++ Task 1/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConfigurationManager configManager = ConfigurationManager.Instance;
+        ConsoleWrapper console = new ConsoleWrapper();
+
+        Console.WriteLine($"Current environment setting: {configManager.EnvironmentSetting ?? "Not set"}");
+
+        Console.Write("Enter a new environment setting: ");
+        string newSetting = console.ReadLine();
+        configManager.EnvironmentSetting = newSetting;
+
+        Console.WriteLine($"Updated environment setting: {configManager.EnvironmentSetting ?? "Not set"}");
+    }
+}
+
+internal class ConsoleWrapper
+{
+    public string ReadLine()
+    {
+        return Console.ReadLine();
+    }
+}
